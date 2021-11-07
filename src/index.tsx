964ca431ec00ea26d95f4e6dd2126f18e5a19f5d
maker27 <evgeniy@maker27.ru>
2021-11-07 17:08:22 +0300

    prepare project

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <div>App</div>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
---
+++ webpack.prod.config.ts
@@ -0,0 +1,72 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import ESLintPlugin from 'eslint-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+
+const config: webpack.Configuration = {
+    mode: 'production',
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].[contenthash].js',
+        publicPath: ''
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
+                    }
+                }
+            },
+            {
+                test: /\.(css|s[ac]ss)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(ttf|eot|woff|woff2|svg)$/,
+                include: path.resolve(__dirname, 'src/fonts'),
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/'
+                    }
+                }
+            },
+            {
+                test: /\.(png|jpe?g|gif|webp|ico|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new MiniCssExtractPlugin(),
+        new ForkTsCheckerWebpackPlugin({
+            async: false
+        }),
+        new ESLintPlugin({
+            extensions: ['js', 'jsx', 'ts', 'tsx']
+        }),
+        new CleanWebpackPlugin()
+    ]
+};
+
+export default config;
