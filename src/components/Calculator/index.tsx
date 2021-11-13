054cf2cdb8b76a563bd3ab881a5354212aa74b6a
maker27 <evgeniy@maker27.ru>
2021-11-13 21:22:50 +0300

    --amend

--- src/components/Calculator/index.tsx
+++ src/components/Calculator/index.tsx
@@ -6,18 +6,20 @@ import { RootState } from '../../store';
 import { removeItem } from '../../store/constructionSlice';
 import { CALCULATOR_DROPPABLE_ID, operations } from '../../assets/constants';
 import { changeDisplay, operationAction } from '../../store/calculatorSlice';
-import { TOperation } from '../../assets/types';
+import { Mode, TOperation } from '../../assets/types';
 import { DraggableWrapper, DroppableWrapper } from '../DragAndDrop';
 
 export default function Calculator(): React.ReactElement {
-    const { items, mode: constructorMode } = useSelector((state: RootState) => state.construction);
+    const { items, mode } = useSelector((state: RootState) => state.construction);
     const dispatch = useDispatch();
     const onRemoveItem = (item: TBoxType) => dispatch(removeItem(item));
     const onOperationAction = (action: TOperation) => dispatch(operationAction(action));
     const onChangeDisplay = (value: string) => dispatch(changeDisplay(value));
 
+    const isRuntimeMode = mode === Mode.runtime;
+
     const onClick = ({ target }: React.MouseEvent<HTMLElement>) => {
-        if (constructorMode) return;
+        if (!isRuntimeMode) return;
         const button = (target as HTMLElement).closest('.button');
         if (button && !button.classList.contains('button-display')) {
             const value = button.textContent;
@@ -32,7 +34,7 @@ export default function Calculator(): React.ReactElement {
     };
 
     const onDoubleClick = ({ target }: React.MouseEvent<HTMLElement>) => {
-        if (!constructorMode) return;
+        if (isRuntimeMode) return;
         const box = (target as HTMLElement).closest('.button-box');
         if (box) {
             const [, boxType] = box.className.match(/box__(\w+)/) || [];
@@ -47,7 +49,7 @@ export default function Calculator(): React.ReactElement {
             isDraggingClassname="droppable">
             <div
                 className="canvas-container"
-                data-fulled={!constructorMode || items.length === Object.keys(boxes).length}
+                data-fulled={isRuntimeMode || items.length === Object.keys(boxes).length}
                 onClick={onClick}
                 onDoubleClick={onDoubleClick}>
                 {items.map((boxType: TBoxType, index) => {
